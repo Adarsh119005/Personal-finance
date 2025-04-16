1909ff9f282a0f1f87fd41785cdc13efe768c4ec
Adarsh Upadhyay <aa7905137620@gmail.com>
2025-04-16 18:41:31 +0530

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { useEffect, useState } from "react";
 import BudgetComparisonChart from "../../component/BudgetComparisonChart";
 import SpendingInsights from "../../component/SpendingInsights";
